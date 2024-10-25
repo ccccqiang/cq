10ca08b14e5ddd1a6df716e94496a4c000fc21b2
ccccqiang <sgmz520@163.com>
2024-10-25 11:47:30 +0800

    refactor(tool):

--- tool/jiami.py
+++ tool/jiami.py
@@ -5,25 +5,25 @@ def encrypt(content, password):
     first_half = content[:half_length]
     second_half = content[half_length:]
 
-    # 使用密码进行加密
+
     encrypted_first_half = ''.join(chr(ord(c) ^ ord(password[i % len(password)])) for i, c in enumerate(first_half))
 
     return encrypted_first_half + second_half
 
-# 输入 Lua 文件路径
-lua_file_path = r'C:\Users\home123\cq\tool\cq.lua'  # 替换为你的 Lua 文件路径
+
+lua_file_path = r'C:\Users\home123\cq\tool\cq.lua'
 desktop_path = os.path.join(os.path.expanduser('~'), 'Desktop', 'encrypted_file.lua')
 
-# 读取 Lua 文件内容
+
 with open(lua_file_path, 'r', encoding='utf-8') as file:
     content = file.read()
 
-# 加密一半内容
+
 password = '123'
 encrypted_content = encrypt(content, password)
 
-# 输出到桌面
+
 with open(desktop_path, 'w', encoding='utf-8') as file:
     file.write(encrypted_content)
 
-print(f"加密后的文件已保存到桌面: {desktop_path}")
+print(f"desktop_path: {desktop_path}")
--- tool/jiemi.py
+++ tool/jiemi.py
@@ -4,25 +4,25 @@ def decrypt(content, password):
     first_half = content[:half_length]
     second_half = content[half_length:]
 
-    # 使用密码进行解密
+
     decrypted_first_half = ''.join(chr(ord(c) ^ ord(password[i % len(password)])) for i, c in enumerate(first_half))
 
     return decrypted_first_half + second_half
 
-# 输入加密的 Lua 文件路径
-encrypted_file_path = r"C:\Users\home123\cq\tool\encrypted_file.lua"  # 替换为你的加密文件路径
+
+encrypted_file_path = r"C:\Users\home123\cq\tool\encrypted_file.lua"
 desktop_path = os.path.join(os.path.expanduser('~'), 'Desktop', 'decrypted_file.lua')
 
-# 读取加密的 Lua 文件内容
+
 with open(encrypted_file_path, 'r', encoding='utf-8') as file:
     content = file.read()
 
-# 解密一半内容
+
 password = '123'
 decrypted_content = decrypt(content, password)
 
-# 输出到桌面
+
 with open(desktop_path, 'w', encoding='utf-8') as file:
     file.write(decrypted_content)
 
-print(f"解密后的文件已保存到桌面: {desktop_path}")
+print(f"desktop_path: {desktop_path}")

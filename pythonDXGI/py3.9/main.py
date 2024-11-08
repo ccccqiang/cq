deb52dfb4e6f451e4b36a7eb66541e4f2f3ac852
ccccqiang <sgmz520@163.com>
2024-11-08 14:42:20 +0800

    refactor(pythonDXGI):调整代码逻辑并移除冗余注释
    - 注释掉打印 adjusted_boxes 的语句- 解除显示检测结果的 cv2.imshow 语句的注释
    - 移除冗余的注释代码块
    - 修复

--- pythonDXGI/py3.9/main.py
+++ pythonDXGI/py3.9/main.py
@@ -100,13 +100,13 @@ while True:
         # 调整坐标以适应全屏
         adjusted_boxes = [(center_x + x, center_y + y, class_id, confidence) for (x, y, class_id, confidence) in
                           detected_boxes]
-        print(adjusted_boxes)
+        # print(adjusted_boxes)
         # 调用鼠标移动函数
         driver = ctypes.CDLL(r'C:\Users\home123\cq\LGMC\logitech.driver.dll')  # 加载驱动
         move_mouse_to_head(adjusted_boxes, driver)
 
-        # # 显示检测结果
-        # cv2.imshow('YOLOv5n ONNX Detection', img_cropped)
+        # 显示检测结果
+        cv2.imshow('YOLOv5n ONNX Detection', img_cropped)
 
         # FPS 计算
         current_time = time.time()
@@ -114,8 +114,8 @@ while True:
         prev_time = current_time
         print(f"FPS: {fps:.2f}")
 
-        # if cv2.waitKey(1) & 0xFF == ord('q'):
-        #     break
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
     except Exception as e:
         print(f"An error occurred: {e}")

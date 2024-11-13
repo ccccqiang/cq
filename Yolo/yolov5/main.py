1711a8d50d9a2f20b72352f686e72b6b7f968883
ccccqiang <sgmz520@163.com>
2024-11-13 10:43:32 +0800

    Auto AIM

--- Yolo/yolov5/main.py
+++ Yolo/yolov5/main.py
@@ -177,14 +177,14 @@ last_f1_state = False
 
 @torch.no_grad()  # 不要删 (do not delete it )
 def find_target(
-        weights=ROOT / 'cs2_fp16.engine',  # model.pt path(s) 选择自己的模型
-        # weights=ROOT / r'C:\Users\home123\cq\pythonDXGI\py3.9\onnx\valorant-n-3.pt',  # model.pt path(s)
+        # weights=ROOT / 'cs2_fp16.engine',  # model.pt path(s) 选择自己的模型
+        weights=ROOT / r'C:\Users\home123\cq\pythonDXGI\py3.9\onnx\valorant-n-3.pt',  # model.pt path(s)
         data=ROOT / 'data/coco128.yaml',  # dataset.yaml path
         imgsz=(320, 320),  # inference size (height, width)
         conf_thres=0.5,  # confidence threshold
         iou_thres=0.45,  # NMS IOU threshold
         max_det=10,  # maximum detections per image
-        device='0',  # cuda device, i.e. 0 or 0,1,2,3 or cpu
+        device='cpu',  # cuda device, i.e. 0 or 0,1,2,3 or cpu
         classes=None,  # filter by class: --class 0, or --class 0 2 3
         agnostic_nms=False,  # class-agnostic NMS
         half=True,  # use FP16 half-precision inference

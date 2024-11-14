5a4ede233eb2035c94bc5e1b60d704cd7cf68ba0
ccccqiang <sgmz520@163.com>
2024-11-15 00:47:22 +0800

    update mouse

--- Yolo/yolov5/app.py
+++ Yolo/yolov5/app.py
@@ -66,4 +66,4 @@ def index():
     return render_template('index.html', configs=configs)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='192.168.124.5', port=5000)

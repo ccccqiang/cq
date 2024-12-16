ca7b24da29bf4a28133532b3a08055bc05aee246
ccccqiang <sgmz520@163.com>
2024-12-16 16:30:43 +0800

    refactor(Yolo): 添加采集卡帧率参数注释
    - 在 main_double.py 文件中，为 device_fps 参数添加注释，说明其代表采集卡帧率
    -此修改提高了代码的可读性和维护性，便于其他开发者理解参数的用途

--- Yolo/yolov5/main_double.py
+++ Yolo/yolov5/main_double.py
@@ -170,7 +170,7 @@ def find_target(
         agnostic_nms=False,  # class-agnostic NMS
         half=True,  # use FP16 half-precision inference
         dnn=False,  # use OpenCV DNN for ONNX inference
-        device_fps=60,
+        device_fps=60, # 采集卡帧率
         use_capture_device = True,  # 设置为 True 表示使用采集卡
         device_index = 0,  # 采集卡索引，默认是0
         mouse_type="CH9350",  # Add mouse type selection here
